9a9e73a4ea24359b69af30a413045327ecd69606
QuangBui <qaker1035@gmail.com>
2022-09-20 15:20:42 +0700

    Add @PreAuthorize

--- src/main/java/com/example/springsecurityjwtexample/api/RoleApi.java
+++ src/main/java/com/example/springsecurityjwtexample/api/RoleApi.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,18 +23,21 @@ public class RoleApi {
         this.roleRepository = roleRepository;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createRole(Role role){
         Role r = roleRepository.save(role);
         return new ResponseEntity<>(new HttpResponse(true, 200, "Create role success", role), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> delete(@PathVariable Long id){
         roleRepository.deleteById(id);
         return new ResponseEntity<>(new HttpResponse(true, 200, "Delete role success", null), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getAll(){
         List<Role> roles = roleRepository.findAll();
